70069da636763436ac6a606d35cd34885b7013ea
Gabriel Weibel <gabriel.weibel@hotmail.de>
2019-05-12 23:21:17 +0200

    Übungen Buch Factorial

---
+++ ModuleBlock2/Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(FactorialIterative(50));
+            Console.WriteLine(FactorialRecursive(50));
+            Console.Read();
+        }
+        private static long FactorialIterative(long n)
+        {
+            long factorialNumber = 1;
+            for (var i = 1; i <= n; i++)
+                factorialNumber *= i;
+            return factorialNumber;
+        }
+        private static long FactorialRecursive(long n)
+        {
+            if (n == 1)
+                return n;
+            return FactorialRecursive(n - 1) * n;
+        }
+    }
+}
