b325083052e4e280adcbdedc58f27a68c900b757
Gabriel Weibel <gabriel.weibel@hotmail.de>
2019-05-16 23:59:19 +0200

    ComplexityTests

--- ModuleBlock3/Maximum/Program.cs
+++ ModuleBlock3/Maximum/Program.cs
@@ -1,18 +1,38 @@
 ﻿using System;
 
 namespace Maximum
-{
+{ 
     internal class Program
     {
+        private static int _count;
         private static void Main()
         {
-            var s = new [] {8, 2, 6, 2, 0, 9};
-            Console.WriteLine(MaximumRecursive(s, 0, s.Length - 1));
+            var s = new int[21];
+            var r = new Random();
+            for (var index = 0; index < s.Length; index++)
+                s[index] = 5;
+
+            MaximumRecursive(s, 0, s.Length - 1);
+            Console.WriteLine(_count);
+            _count = 0;
+            MaximumIterative(s);
+            Console.WriteLine(_count);
             Console.Read();
         }
 
+        private static int MaximumIterative(int[] s)
+        {
+            var max = s[0];
+            foreach (var number in s)
+            {
+                max = Math.Max(max, number);
+                _count++;
+            }
+            return max;
+        }
         private static int MaximumRecursive(int[] s, int left, int right)
         {
+            _count++;
             if (left == right)
                 return s[left];
             var middle = (left + right) / 2.0;
