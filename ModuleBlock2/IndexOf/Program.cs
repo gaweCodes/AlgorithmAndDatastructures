dc7e07fdb6641402aff26a7de484e123895c022e
Gabriel Weibel <gabriel.weibel@hotmail.de>
2019-06-01 11:58:45 +0200

    Additional Tasks

---
+++ ModuleBlock2/IndexOf/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IndexOf
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(IndexOfIterative('S', "HAUS"));
+            Console.WriteLine(IndexOfIterative('K', "HAUS"));
+            Console.WriteLine(IndexOfRecursive('S', "HAUS"));
+            Console.WriteLine(IndexOfRecursive('K', "HAUS"));
+            Console.ReadLine();
+        }
+        private static int IndexOfIterative(char needle, string haystack)
+        {
+            for (var index = 0; index < haystack.Length; index++)
+            {
+                if (haystack[index] == needle)
+                    return index;
+            }
+            return -1;
+        }
+        private static int IndexOfRecursive(char needle, string haystack)
+        {
+            var length = haystack.Length;
+            if (length == 0)
+                return -1;
+
+            if (haystack[length - 1] == needle)
+                return length - 1;
+            return IndexOfRecursive(needle, haystack.Substring(0, length - 1));
+        }
+    }
+}
---
+++ ModuleBlock2/SumDifference/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SumDifference
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(SumDifferenceIterative(1,5));
+            Console.WriteLine(SumDifferenceRecursive(1, 5));
+            Console.ReadLine();
+        }
+        private static int SumDifferenceIterative(int min, int max)
+        {
+            var sum = 0;
+            while (min <= max)
+            {
+                sum += min;
+                min++;
+            }
+            return sum;
+        }
+        private static int SumDifferenceRecursive(int min, int max, int result = 0)
+        {
+            if (min > max)
+                return result;
+            result += min;
+            return SumDifferenceRecursive(++min, max, result);
+        }
+    }
+}
